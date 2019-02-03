c8926477ec667696947528640d0c006144a221fe
friosl <38120585+friosl@users.noreply.github.com>
2019-02-02 20:49:23 -0500

    Add files via upload

--- laboratorios/lab01/codigo/taller1.py
+++ laboratorios/lab01/codigo/taller1.py
@@ -21,6 +21,7 @@ class Bicolorable:
 		self.divideArray(self.Array)
 
 	def divideArray(self,Array):
+		print("Aquí imprime el arreglo completo supuestamente")
 		print(self.Array)		
 		self.dict = {}
 
@@ -28,23 +29,53 @@ class Bicolorable:
 		for i in range (len(self.Array)):
 			pos1 = Array[i]
 			self.hash(pos1)
-		#self.comparison()
+		self.evaluate()
 
 	def hash(self,p1):
 		#Mental note(We use self because the method is part of the class and not of the objects)
 		#Here we create the dictionary to assign every number an edge 
+		self.pwd=0
+		self.bicol=True
 
 		a,b = p1.split(" ")
+
 		if int(a) in self.dict:
 			self.dict[int(a)].append(int(b))
-			print("añadiendo mas veces")
 		else:
-			self.dict[int(a)] = [int(b)] 
+			self.dict[int(a)] = [int(b)]
+
+		if int(b) in self.dict:
+			self.dict[int(b)].append(int(a))
+		else:
+			self.dict[int(b)] = [int(a)]
+
 		print(self.dict[int(a)])
-		print("añadiendo por primera vez")
+	
+	def adyacency(self, list, val):
 
+		#if (self.pwd in self.dict[num]):
+		#	self.bicol=False
+		#	print("No Bicolorable")
+		return not val in list
 
-	#def comparison():
+	def evaluate(self):
+		bicol = False
+		keys = self.dict.keys()
+		for i in keys:
+			adjs = self.dict[i]
+			#self.pwd = int(self.dict[i][0])
+			if len(adjs)>1:
+				aux = 0
+				for j in adjs:
+					for k in adjs:
+						if j != k:
+							bicol = self.adyacency(self.dict[j],k)
+
+		if bicol:
+			print("Bicolorable")
+		else:
+			print("Not Bicolorable")
+
 
 
 n = Bicolorable()
