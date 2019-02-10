7614ee1105a94d5dcf1de46e868bb5475fafafe9
friosl <38120585+friosl@users.noreply.github.com>
2019-02-10 01:05:59 -0500

    Update reinas.java

--- talleres/taller03/reinas.java
+++ talleres/taller03/reinas.java
@@ -1,4 +1,4 @@
-*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -31,6 +31,9 @@ public static int contador=0;
                     tablero[c]=i;
                     if(nReinas(n, tablero, c + 1)) return true;
                 }
+            }
+            if (false){
+                            nReinas(n,tablero,c-1);
             }
 
         }
