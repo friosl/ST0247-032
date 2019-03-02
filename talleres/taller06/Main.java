4ca31d8db8c7b120b800240a7e6c944aaa92736c
friosl <38120585+friosl@users.noreply.github.com>
2019-03-02 00:00:31 -0500

    Create Main.java

---
+++ talleres/taller06/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    int[] i = {2000,1000,500,200,100,50};
+    for (int c: Taller6.cambioGreedy(5200,i))
+        System.out.println(c);
+  }
+}
